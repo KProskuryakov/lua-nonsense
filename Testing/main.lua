69dcf45586e4b34a159d21d494e9d4606fdac8b7
Kostyantyn Proskuryakov <kostyapro@gmail.com>
2016-05-20 14:38:40 -0400

    The start of something silly

---
+++ Testing/main.lua
@@ -0,0 +1,9 @@
+function love.load(arg)
+  if arg[#arg] == "-debug" then require("mobdebug").start() end     -- enable debug mode for ZeroBrane Studio`
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
